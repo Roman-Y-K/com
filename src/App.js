f36999c2974fbd77ff5a50d2dbfc9ec285cbaa78
Roman-Y-K <r.prohor7@gmail.com>
2021-08-06 14:40:10 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
+import CommentsForm from './Components/CommentsForm';
+import CommentsList from './Components/CommentsList/';
+import Navigation from './Components/Navigation';
 
+import styles from './app.module.css';
 
 function App() {
   return (
-   <div></div>
+    <div className={styles.container}>
+      <CommentsForm />
+      <CommentsList />
+      <Navigation />
+    </div>
   );
 }
 
